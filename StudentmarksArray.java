32604bd03e4ac074b0a7229c3ddfae9c3e0bdbff
Sudhanshu Pandey <sudh2005anshu@gmail.com>
2026-04-10 19:15:46 +0530

    Student Mark Program

---
+++ StudentmarksArray.java
@@ -0,0 +1,18 @@
+//Store the marks of five student using loop
+
+public class StudentmarksArray{
+   public static void main(String[] args){
+    //Create an array for storing the element of array 
+   int []marks = {90,98,97,95,96,94,96,100,99,92};
+
+   for(int i=0; i < marks.length; i++){
+   // System.out.println("marks of student" + marks[i]);
+     System.out.println( "Student" + " " +(i+1)  +" " +"Marks="  +marks[i]);
+   }
+
+}
+}
+
+
+    
+
